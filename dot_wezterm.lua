c7de9d24f9552cbfa2c8edc88c8ebc9c2b42fcf4
Kousuke Ebihara <kousuke@co3k.org>
2023-05-09 12:22:32 +0900

    ..

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,10 +6,6 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  config.default_prog = {"wsl.exe", "~", "-d", "Ubuntu-22.04"}
-end
-
 config.ssh_domains = {
   {
     name = 'ebimen',
@@ -22,5 +18,16 @@ config.ssh_domains = {
     username = 'co3k',
   },
 }
+
+config.unix_domains = {
+  {
+    name = 'wsl',
+    serve_command = { 'wsl', 'wezterm-mux-server', '--daemonize' },
+  },
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.default_gui_startup_args = { 'connect', 'wsl' }
+end
 
 return config
